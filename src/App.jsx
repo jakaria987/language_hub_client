3ef30891ce743cc692d41ac6d9cee0d5f76347bb
Md Jakaria <jakhossain999@gmail.com>
2023-06-07 02:51:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button className="btn btn-outline btn-success">Success</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h2> this is header</h2>
+        </div>
+    );
+};
+
+export default Footer;
