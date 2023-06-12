f6ed0b4627cb665abf38f68720e9dde6605fa6bf
Md Jakaria <jakhossain999@gmail.com>
2023-06-12 07:53:15 +0600

    add footer

--- src/Pages/Home/Xtraa/Xtraa.jsx
+++ src/Pages/Home/Xtraa/Xtraa.jsx
@@ -18,7 +18,7 @@ const Xtraa = () => {
         <img data-aos="fade-up" className='mx-auto' src={pic} alt="" />
       <h1 data-aos="fade-up" className='text-teal-900 font-semibold'>Welcome to our language club</h1>
       
-      <p data-aos="fade-up" data-aos-delay="200" className='text-teal-900 font-semibold'>
+      <p data-aos="fade-up" data-aos-delay="600" className='text-teal-900 font-semibold'>
       What transferable skills should you include on the CV?  On the previous article we discussed “what are transferable skills”, to recap quickly, here is the definition. Transferable skills are a core set of skills and abilities, which you have learnt and perfected over time, gained from previous jobs, voluntary and charity work, your hobbies and from home. Both Soft skills and Hard skills are transferable skills.
       </p>
       <hr className='border-teal-900 w-2/3 mx-auto' />
